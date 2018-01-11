c8376dbe0018887f47d4e1cb3c6e4289d7607e15
chumphrey77 <colten.humphrey@eagles.oc.edu>
2018-01-10 23:10:45 -0600

    I added a comment

--- app/src/main/java/com/goose77/router2/MainActivity.java
+++ app/src/main/java/com/goose77/router2/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Heyo I made a comment
+        //Heyo I made a comment!
     }
 }
