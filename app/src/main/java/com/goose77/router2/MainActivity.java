46a62fb0286f87c7dbc843fc0c7bf9cdf1db1061
chumphrey77 <colten.humphrey@eagles.oc.edu>
2018-01-21 15:24:03 -0600

    Created the user interface elements that will be used for the remainder of this project

--- app/src/main/java/com/goose77/router2/MainActivity.java
+++ app/src/main/java/com/goose77/router2/MainActivity.java
@@ -2,7 +2,12 @@ package com.goose77.router2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.goose77.router2.UI.UIManager;
+import com.goose77.router2.networks.Constants;
 import com.goose77.router2.support.Bootloader;
 
 /**
@@ -15,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Bootloader(this);
+    }
+
+    //Add the Show IP Address button to the menu
+    //When the button is clicked show the message with the devices IP Address
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        UIManager uiManager = UIManager.getInstance();
+        if (item.getItemId() == R.id.showIPAddress){
+            uiManager.displayMessage("Your IP address is "+ Constants.IP_ADDRESS);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //Create the Menu when the app is loaded
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
